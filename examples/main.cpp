d1624814f7f7336def5404fb8e5a732cecfb2074
the-hyp0cr1t3 <hriday.gajulapalli@gmail.com>
2022-11-08 23:05:27 +0530

    Pixel!

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,6 +1,10 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include<vector>
+
+#define WIDTH 800
+#define HEIGHT 600
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
     glViewport(0, 0, width, height);
@@ -10,13 +14,20 @@ void processInput(GLFWwindow *window) {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) glfwSetWindowShouldClose(window, true);
 }
 
+void makePixel(std::vector<GLubyte>& pixelBuffer, unsigned int x, unsigned int y) {
+    int position = (x + y*WIDTH) * 3;
+    pixelBuffer[position] = 255;
+    pixelBuffer[position + 1] = 255;
+    pixelBuffer[position + 2] = 255;
+}
+
 int main() {
     glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
 
-    GLFWwindow *window = glfwCreateWindow(800, 600, "DontLearnOpenGL", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "DontLearnOpenGL", NULL, NULL);
     if (window == NULL) {
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
@@ -36,6 +47,16 @@ int main() {
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        std::vector<GLubyte> pixelBuffer(WIDTH*HEIGHT*3);
+        makePixel(pixelBuffer, 20, 30);
+        glDrawPixels(WIDTH, HEIGHT, GL_RGB, GL_UNSIGNED_BYTE, pixelBuffer.data());
+
+        // GLint x = 200, y = 200;
+
+        // glBegin(GL_POINTS);
+        // glVertex2i(200, 200);
+        // glEnd();
 
         glfwSwapBuffers(window);
         glfwPollEvents();
